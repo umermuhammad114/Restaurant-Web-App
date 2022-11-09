122b058f51b815fb26e804d74da7863a6f308b77
Syed Umer <umermuhammad114@gmail.com>
2022-11-09 17:25:05 +0500

    Promises Part 1

--- src/app/services/promotion.service.ts
+++ src/app/services/promotion.service.ts
@@ -9,15 +9,15 @@ export class PromotionService {
 
   constructor() { }
 
-  getPromotions(): Promotion[] {
-    return PROMOTIONS;
+  getPromotions(): Promise<Promotion[]> {
+    return Promise.resolve(PROMOTIONS);
   }
 
-  getDish(id: string): Promotion {
-    return PROMOTIONS.filter((promotion) => promotion.id === id)[0];
+  getDish(id: string): Promise<Promotion> {
+    return Promise.resolve(PROMOTIONS.filter((promotion) => promotion.id === id)[0]);
   }
 
-  getFeaturedPromotion(): Promotion {
-    return PROMOTIONS.filter((promotion) => promotion.featured)[0];
+  getFeaturedPromotion(): Promise<Promotion> {
+    return Promise.resolve(PROMOTIONS.filter((promotion) => promotion.featured)[0]);
   }
 }
